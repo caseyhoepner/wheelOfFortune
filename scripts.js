520eaec9c1784296490f463b3d19d4effd9d4b61
Casey Hoepner <caseyhoepner@gmail.com>
2019-08-14 11:40:22 +0100

    Show and hide welcome page and game view on click of start game

--- scripts.js
+++ scripts.js
@@ -1,3 +1,13 @@
-var startBtn = document.querySelector('.start-btn');
+const startBtn = document.querySelector('.start-btn');
 
-console.log(startBtn);
+startBtn.addEventListener('click', startGame);
+
+function startGame() {
+  const gameElements = document.querySelector('.game-view');
+  const welcomePage = document.querySelector('.welcome-page');
+
+  gameElements.classList.remove('hide');
+  welcomePage.classList.add('hide');
+  
+}
+
